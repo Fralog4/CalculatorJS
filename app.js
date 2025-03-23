521c2e803056d8bb777243b77ce4f29ca586984a
Fralog4 <flogullo.bio@yahoo.com>
2025-03-23 22:10:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const addButton= document.querySelector('#addButton');
+const equalButton= document.querySelector('#equalsButton');
+const clearButton= document.querySelector('#clearButton');
+const subtractButton= document.querySelector('#minusButton');
